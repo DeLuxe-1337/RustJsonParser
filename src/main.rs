781d6806a8f1f3c523994a5c113a3258750840b6
DeLuxe1337001 <rragtagbates@gmail.com>
2022-02-01 14:07:58 -0500

    Timer

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ mod token_type;
 
 mod lexer;
 
+use std::time::{Duration, Instant};
+
 fn main() {
+    let now = Instant::now();
+
     let source = std::fs::read_to_string("D:\\Rust\\JsonParser\\src\\my_json.json").expect("Expected file");
 
     let mut lexer = lexer::lexer_t::new(source.as_str());
@@ -12,4 +16,8 @@ fn main() {
     for token in lexer.tokens {
         println!("Token: {:?}, {1}, {2}", token.tok_type, token.value, token.line);
     }
+
+    println!("It took {} ms to run!", now.elapsed().as_millis());
+
+    std::io::stdin().read_line(&mut String::new());
 }
